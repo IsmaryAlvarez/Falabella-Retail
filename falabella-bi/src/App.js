a45cd00dc26673d8c7475dadf5f51c1ec0695c38
IsmaryAlvarez <ismaryad@gmail.com>
2018-04-03 12:43:27 -0300

    menu

--- falabella-bi/src/App.js
+++ falabella-bi/src/App.js
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
-import logo from './img/logo-cuadrado-falabella.jpg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./img/logo-cuadrado-falabella.jpg";
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Falabella-BI</h1>
         </header>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">React-Bootstrap</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              Link
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link
+            </NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>;
       </div>
     );
   }
